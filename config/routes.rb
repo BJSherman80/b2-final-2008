ca8038a26e4a44465c12fec562fa4bd75698ef95
Brett Turing <bjsherman80@gmail.com>
2020-11-09 11:14:34 -0700

    addd resources patients only :index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:show]
-
+  resources :patients, only: [:index]
   resources :hospitals, only: [:show]
 
   delete '/doctors/:doc_id/patients/:patient_id', to: 'appointments#destroy'

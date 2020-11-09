47712f8a34137dcba57e6938ef9a7e6458bf557a
Brett Turing <bjsherman80@gmail.com>
2020-11-09 10:55:29 -0700

    add route for deleting a patient from a doctor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:show]
+
   resources :hospitals, only: [:show]
+
+  delete '/doctors/:doc_id/patients/:patient_id', to: 'appointments#destroy'
 end

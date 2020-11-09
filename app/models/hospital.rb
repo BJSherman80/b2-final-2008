0db07c3d06b893035cc1c3be97b88202edaf898b
Brett Turing <bjsherman80@gmail.com>
2020-11-09 10:16:09 -0700

    create model methods for hospital

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,13 @@
 class Hospital < ApplicationRecord
   has_many :doctors
   has_many :patients, through: :doctors
+
+  def doc_count
+    self.doctors.count.to_i
+  end
+
+  def doc_universities
+    self.doctors.pluck(:university).uniq
+  end
+
 end

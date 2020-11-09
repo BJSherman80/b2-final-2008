07ac4a1881e5bacc11eec3fdb517a96f23e14215
Brett Turing <bjsherman80@gmail.com>
2020-11-09 10:15:02 -0700

    create test for .doc_count and .doc_universities

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Hospital, type: :model do
   it { should have_many :doctors }
   it { should have_many(:patients).through(:doctors) }
  end
+
+ describe 'instance methods' do
+   before(:each) do
+     @hospital = Hospital.create!(name: "Banner Health AZ")
+     @doctor_1 = @hospital.doctors.create!(name: "Blake Sherman", specialty: "ER Doc", university: "CU Denver Anchutz")
+     @doctor_2 = @hospital.doctors.create!(name: "Hayden Sherman", specialty: "Heart Doc", university: "ASU")
+   end
+   it '.doc_count' do
+     expect(@hospital.doc_count).to eq(2)
+   end
+
+   it '.doc_universities' do
+     expect(@hospital.doc_universities).to eq([@doctor_1.university, @doctor_2.university])
+   end
+ end
 end

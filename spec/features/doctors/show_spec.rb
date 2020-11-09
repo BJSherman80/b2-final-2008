4a6ec3ba784e0805a9a45ef83121a4161d018f6b
Brett Turing <bjsherman80@gmail.com>
2020-11-09 09:53:25 -0700

    doctor show page spec

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+
+RSpec.describe 'Doctors show page', type: :feature do
+  it 'can see doc info and hospital they work at along with patients they have info' do
+    hospital = Hospital.create!(name: "Banner Health AZ")
+    doctor_1 = hospital.doctors.create!(name: "Blake Sherman", specialty: "ER Doc", university: "CU Denver Anchutz")
+    patient_1 = Patient.create!(name: "Brett Sherman", age: '20')
+    patient_2 = Patient.create!(name: "Chad Sipe", age: '28')
+    doctor_1.patients << patient_1
+    doctor_1.patients << patient_2
+
+    visit "/doctors/#{doctor_1.id}"
+    save_and_open_page
+    expect(page).to have_content(doctor_1.name)
+    expect(page).to have_content(doctor_1.specialty)
+    expect(page).to have_content(doctor_1.university)
+    expect(page).to have_content(patient_1.name)
+    expect(page).to have_content(patient_2.name)
+  end
+end

fb4451bfb2927efe71de0b42f968868758d57ae3
Brett Turing <bjsherman80@gmail.com>
2020-11-09 09:24:40 -0700

    test all models and relationships

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Hospital, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :doctors }
+  it { should have_many(:patients).through(:doctors) }
 end
